9447d03e553ef2da957b3acdcd4a42b715e1ef05
Eric E Monson <emonson@cs.duke.edu>
2012-10-04 13:04:06 -0400

    Initial Commit

---
+++ TinyXMLtest1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TinyXMLtest1
+//
+//  Created by Eric Monson on 10/4/12.
+//  Copyright (c) 2012 Duke University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
